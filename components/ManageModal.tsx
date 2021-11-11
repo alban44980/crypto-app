5aea95a1faa2f5e79ced0cd57980ebd9f335b7b9
Alban Mansord <alban.mansord@gmail.com>
2021-11-11 22:48:48 +0100

    Manage Modal structure and Basic Styling

--- components/ManageModal.tsx
+++ components/ManageModal.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { View, StyleSheet, Modal, TouchableOpacity, Text } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Modal,
+  TouchableOpacity,
+  Text,
+  SafeAreaView,
+} from 'react-native';
 
 function ManageModal({ manageModal, setManageModal }: any) {
   return (
@@ -9,16 +16,32 @@ function ManageModal({ manageModal, setManageModal }: any) {
       visible={manageModal}
       onRequestClose={() => {}}
     >
-      <View style={styles.container}>
-        <Text>HELLO FROM MANAGE</Text>
-        <TouchableOpacity
-          onPress={() => {
-            setManageModal(false);
-          }}
-        >
-          <Text>BACK BUTTON</Text>
-        </TouchableOpacity>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.backContainer}>
+          <TouchableOpacity
+            style={styles.backButton}
+            onPress={() => {
+              setManageModal(false);
+            }}
+          >
+            <Text style={styles.backText}>BACK</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.titleContainer}>
+          <Text style={styles.titleText}>MY INVESTMENTS</Text>
+        </View>
+        <View style={styles.dataContainer}></View>
+        <View style={styles.confirmContainer}>
+          <TouchableOpacity
+            style={styles.confirmButton}
+            onPress={() => {
+              // setAddModal(false);
+            }}
+          >
+            <Text style={styles.confirmText}>Add</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </Modal>
   );
 }
@@ -27,8 +50,59 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#E6E8FA',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  backContainer: {
+    height: '7%',
+    width: '100%',
+    // backgroundColor: 'red',
+  },
+  backButton: {
+    backgroundColor: '#FFF7EF',
+    marginLeft: 'auto',
+    marginTop: 5,
+    marginRight: 5,
+    height: '70%',
+    width: '30%',
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 20,
+  },
+  backText: {},
+  titleContainer: {
+    // backgroundColor: 'yellow',
+    height: '13%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleText: {
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  dataContainer: {
+    height: '55%',
+    backgroundColor: '#FDF9F5',
+    margin: 30,
+    borderRadius: 20,
+  },
+  confirmContainer: {
+    // backgroundColor: 'red',
+    height: '15%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  confirmButton: {
+    backgroundColor: '#FFF7EF',
+    height: '50%',
+    width: '55%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+  },
+  confirmText: {
+    fontSize: 25,
+    fontWeight: 'bold',
   },
 });
 
