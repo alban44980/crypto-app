65e655a63fef1398adedd8dfd43abfa794090c45
Alban Mansord <alban.mansord@gmail.com>
2021-11-11 19:50:20 +0100

    Main Bottom structure

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import MainTop from './components/MainTop';
-import BottomTop from './components/BottomTop';
+import MainBottom from './components/MainBottom';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <MainTop />
-      <BottomTop />
+      <MainBottom />
     </SafeAreaView>
   );
 }
---
+++ components/MainBottom.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+function MainBottom() {
+  return (
+    <View style={styles.bottom}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.investTitle}>My investments</Text>
+      </View>
+      <View style={styles.dataContainer}></View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.manageButton}>
+          <Text style={styles.manageText}>Manage</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  bottom: {
+    height: '50%',
+    width: '100%',
+    backgroundColor: '#E6E8FA',
+    alignItems: 'center',
+  },
+  titleContainer: {
+    // backgroundColor: 'green',
+    width: '100%',
+    height: '15%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  investTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  dataContainer: {
+    backgroundColor: 'red',
+    height: '55%',
+    width: '100%',
+  },
+  buttonContainer: {
+    // backgroundColor: 'aquamarine',
+    height: '30%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  manageButton: {
+    backgroundColor: '#FFF7EF',
+    width: '60%',
+    height: '50%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+  },
+  manageText: {
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+});
+
+export default MainBottom;
